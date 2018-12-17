5c5dc35331f8ec24935dab955192aa538b4b3cef
DeepalakshmiR-hexaware <45679339+DeepalakshmiR-hexaware@users.noreply.github.com>
2018-12-17 16:27:07 +0530

    update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 
 
   app.post('/', function (req, res) {
-  console.log(JSON.stringify(req.body);
+  console.log(JSON.stringify(req.body));
     res.send('POST request to the homepage');
   });
 app.listen(8080, () => console.log("listening.."));
