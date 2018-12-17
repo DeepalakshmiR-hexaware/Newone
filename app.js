1ed20c1497fc18534dbd3bf662d2e7e90f96a5fd
DeepalakshmiR-hexaware <45679339+DeepalakshmiR-hexaware@users.noreply.github.com>
2018-12-17 19:11:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,31 @@ var app = express();
 
   app.post('/', function (req, res) {
   console.log(JSON.stringify(req.body));
-    res.send('POST request to the homepage');
+  if(req.body.queryResult.intent.displayName =="Ticket"){
+    res.JSON(
+      {
+        "conversationToken": "",
+        "expectUserResponse": true,
+        "expectedInputs": [
+            {
+                "inputPrompt": {
+                    "richInitialPrompt": {
+                        "items": [
+                            {
+                                "simpleResponse": {
+                                    "textToSpeech": "Howdy! I can tell you fun facts about almost any number, like 42. What do you have in mind?",
+                                    "displayText": "Howdy! I can tell you fun facts about almost any number. What do you have in mind?"
+                                }
+                            }
+                        ]
+                        
+                    }
+                }
+                
+            }
+        ]
+    }
+    )
+  }  
   });
 app.listen(process.env.PORT, () => console.log("listening.."));
