60f2ee189536c7e6add65baea3faace352efe6ac
DeepalakshmiR-hexaware <45679339+DeepalakshmiR-hexaware@users.noreply.github.com>
2018-12-19 14:52:47 +0530

    updated app.js

--- app.js
+++ app.js
@@ -10,45 +10,41 @@ var bodyParser = require('body-parser');
   if(req.body.queryResult.intent.displayName == "test"){
     console.log("ddd");
     res.json({
-  "payload": {
-    "google": {
-      "expectUserResponse": true,
-      "richResponse": {
-        "items": [
-          {
-            "simpleResponse": {
-              "textToSpeech": "this is a simple response"
-            }
+      "payload": {
+        "google": {
+          "expectUserResponse": true,
+          "richResponse": {
+            "items": [
+              {
+                "simpleResponse": {
+                  "textToSpeech": "this is a simple response"
+                }
+              }
+            ]
           }
-        ]
+        }
       }
-    }
-  }
-});
+    });
   }
   else if(req.body.queryResult.intent.displayName =="Ticket"){
     res.json(
       {
-        "expectUserResponse": true,
-        "expectedInputs": [
-            {
-                "inputPrompt": {
-                    "richInitialPrompt": {
-                        "items": [
-                            {
-                                "simpleResponse": {
-                                    "textToSpeech": "Howdy! I can tell you fun facts about almost any number, like 42. What do you have in mind?",
-                                    "displayText": "Howdy! I can tell you fun facts about almost any number. What do you have in mind?"
-                                }
-                            }
-                        ]
-                        
-                    }
+        "payload": {
+          "google": {
+            "expectUserResponse": true,
+            "richResponse": {
+              "items": [
+                {
+                  "simpleResponse": {
+                    "textToSpeech": "this is a simple response"
+                  }
                 }
-                
+              ]
             }
-        ]
-    }
+          }
+        }
+      }
+     
     )
   }  
   });
