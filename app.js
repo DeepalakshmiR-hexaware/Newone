0e2d84e48e864c0292714f88a8b0dc1454935135
Deepalakshmi Rajendiran <42936@corp.hexaware.com>
2018-12-24 19:54:55 +0530

    updated

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ var bodyParser = require('body-parser');
                   "basicCard": {
                     "title": "Booking process",
                     "subtitle":ticketObj.parameters.TicketType,
-                    "formattedText":"FROM:"+ticketObj.parameters.from+"TO:"+ticketObj.parameters.to
+                    "formattedText":"FROM:"+ticketObj.parameters.from.name+"TO:"+ticketObj.parameters.to.name
                   }
                 }
               ]
