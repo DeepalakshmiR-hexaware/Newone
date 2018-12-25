a6d0a21d7d5c06b7a60e13e96e548a07a22e7cce
Deepalakshmi Rajendiran <42936@corp.hexaware.com>
2018-12-25 16:10:38 +0530

    updated

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var bodyParser = require('body-parser');
             "items": [
               {
                 "simpleResponse": {
-                  "textToSpeech":"tested",
+                  "ssml":"<speak>tested <say-as interpret-as=\"characters\">SSML</say-as></speak>",
                   "displayText": "tested"
                 }
               }
@@ -41,50 +41,6 @@ var bodyParser = require('body-parser');
                   }
                 }
               ]
-            },
-            "systemIntent": {
-              "intent": "actions.intent.OPTION",
-              "data": {
-                "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-                "listSelect": {
-                  "title": "Hello",
-                  "items": [
-                    {
-                      "optionInfo": {
-                        "key": "first title key"
-                      },
-                      "description": "first description",
-                      "image": {
-                        "url": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-                        "accessibilityText": "first alt"
-                      },
-                      "title": "first title"
-                    },
-                    {
-                      "optionInfo": {
-                        "key": "second"
-                      },
-                      "description": "second description",
-                      "image": {
-                        "url": "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
-                        "accessibilityText": "second alt"
-                      },
-                      "title": "second title"
-                    },
-                    {
-                      "optionInfo": {
-                        "key": "three"
-                      },
-                      "description": "third description",
-                      "image": {
-                        "url": "https://www.google.com/logos/doodles/2018/holidays-2018-northern-hemisphere-day-3-5708260869931008-law.gif",
-                        "accessibilityText": "merry christmas"
-                      },
-                      "title": "merry christmas"
-                    }
-                  ]
-                }
-              }
             }
           }
         }
