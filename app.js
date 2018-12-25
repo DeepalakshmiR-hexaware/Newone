77e3135ff728ba4bb403559265f80d7ebdf7bf24
Deepalakshmi Rajendiran <42936@corp.hexaware.com>
2018-12-25 14:53:17 +0530

    updated

--- app.js
+++ app.js
@@ -38,9 +38,41 @@ var bodyParser = require('body-parser');
                   "simpleResponse": {
                     "textToSpeech": "Provide depart date",
                     "displayText": "Provide depart date"
-                  }
+                  },
                 }
               ]
+            },
+            "systemIntent": {
+              "intent": "actions.intent.OPTION",
+              "data": {
+                "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+                "carouselSelect": {
+                  "items": [
+                    {
+                      "optionInfo": {
+                        "key": "first title"
+                      },
+                      "description": "first description",
+                      "image": {
+                        "url": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+                        "accessibilityText": "first alt"
+                      },
+                      "title": "first title"
+                    },
+                    {
+                      "optionInfo": {
+                        "key": "second"
+                      },
+                      "description": "second description",
+                      "image": {
+                        "url": "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
+                        "accessibilityText": "second alt"
+                      },
+                      "title": "second title"
+                    }
+                  ]
+                }
+              }
             }
           }
         }
