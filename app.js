6636a13f34baf8d4941d3645f5e03ebb8db3ca79
Deepalakshmi Rajendiran <42936@corp.hexaware.com>
2018-12-27 16:38:44 +0530

    updated

--- app.js
+++ app.js
@@ -191,7 +191,7 @@ var jsonData = require('./list.json');
  else if(req.body.queryResult.action == "BookingList.BookingList-custom"){
    console.log("okk",JSON.stringify(req.body));
   var showItems = [];
-  for( var i=0; i<jsonData.Bookings.length; i++){  
+  for( var i=0; i<jsonData.Bookings.length+1; i++){  
   if(req.body.queryResult.parameters.number == jsonData.Bookings[i].id){  
     showItems.push({
       "simpleResponse":{
