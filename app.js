83a9e6be58b871b0ce1a0bd6cfc09522feb1d7da
Deepalakshmi Rajendiran <42936@corp.hexaware.com>
2018-12-27 18:14:28 +0530

    updated

--- app.js
+++ app.js
@@ -191,9 +191,15 @@ var jsonData = require('./list.json');
  else if(req.body.queryResult.action == "BookingList.BookingList-custom"){
    console.log("okk",JSON.stringify(req.body));
   var showItems = [];
+  if(req.body.queryResult.parameters.number.length>0){
+    num = req.body.queryResult.parameters.number;
+  }else if(req.body.queryResult.parameters.num.length>0){
+    num = req.body.queryResult.parameters.num;
+  }
   for( var i=0; i<jsonData.Bookings.length; i++){  
     console.log("number",req.body.queryResult.parameters.number);
-  if(req.body.queryResult.parameters.number == jsonData.Bookings[i].id){  
+
+  if(num == jsonData.Bookings[i].id){  
     showItems.push({
       "simpleResponse":{
       "textToSpeech": "FROM:"+jsonData.Bookings[i].from+" TO:"+jsonData.Bookings[i].to,
